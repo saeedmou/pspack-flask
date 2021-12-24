fb6bbee3f281511a4db3c58acf3834ed78c07f7e
Saeed Mousavian <saeed.mousavian@gmail.com>
2021-12-24 09:34:55 +0000

    LED & Relay pins and functions defined

--- switcher.py
+++ switcher.py
@@ -1,17 +1,40 @@
 from gpiozero import LED
 from signal import pause
-led = LED(4)
+relayObj = LED(4)
+led1 = LED(17)
+led2 = LED(27)
 
-def myLED():
-    led.blink()
-
-
-def myLEDOn():
+def relay(state=False):
     # led.blink()
-    led.on()
-    return "On"
+    output="USB ON"
+    if(state==True):
+        relayObj.on()
+    else:
+        relayObj.off()
+        output="USB OFF"
+    return output
 
-def myLEDOff():
-    # led.blink()
-    led.off()
-    return "Off"
+def led(index=1,state=False):
+    if(index==1):
+        output=LED1(state)
+    else:
+        output=LED2(state)
+    return output
+
+def LED1(state=False):
+    output="LED 1 ON"
+    if(state==True):
+        led1.on()
+    else:
+        led1.off()
+        output="LED 1 OFF"
+    return output
+
+def LED2(state=False):
+    output="LED 2 ON"
+    if(state==True):
+        led2.on()
+    else:
+        led2.off()
+        output="LED 2 OFF"
+    return output
