034fdc06666a6878a0998ac906eee209481d9984
Saeed Mousavian <saeed.mousavian@gmail.com>
2022-03-29 08:37:19 +0100

    send search parameters to log

--- static/logging.js
+++ static/logging.js
@@ -1,9 +1,21 @@
-function logMessage(msg)
-{
+function logMessage(msg) {
+        // This Part added to use with diffrent GoldHEN Payloads.
+        const queryString = window.location.search;
+        const url = new URL(window.location.origin +'/log/' + encodeURIComponent(msg));
+        url.search=queryString;
+        
+        // const urlParams = new URLSearchParams(queryString);
+        // urlParams.forEach((value,key) => {
+        //         url.searchParams.set(key, value);
+        // });
+        // console.log(url);
+
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', './log/' + encodeURIComponent(msg), true);
+        xhr.open('GET', url, true);
         xhr.onerror = function () {
                 alert("Failed to log: message" + msg)
         }
         xhr.send();
 }
+
+
